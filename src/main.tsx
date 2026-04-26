4be5dcbf76a2a052804b4ad856ab1fd7ba3f0af0
michalinamusielik <mmusielik@gmail.com>
2026-04-26 18:38:28 +0200

    style: refactor design system with new typography and color palette
    Agent-Id: agent-6dada338-2f4c-4347-bbc2-a929f6a02d34
    Linked-Note-Id: 9e82dcc3-b8d9-4fa3-a753-f4558d5542c7

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource-variable/fraunces/full.css'
+import '@fontsource-variable/fraunces/full-italic.css'
+import '@fontsource/source-serif-4/400.css'
+import '@fontsource/source-serif-4/400-italic.css'
+import '@fontsource/source-serif-4/600.css'
+import '@fontsource/source-serif-4/700.css'
+import '@fontsource-variable/inter/wght.css'
+import '@fontsource-variable/inter/wght-italic.css'
 import './index.css'
 import App from './App.tsx'
 
